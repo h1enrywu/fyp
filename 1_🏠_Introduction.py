99c4eabdd602ec3fabcd1dca96022eafaeeded21
h1enrywu <s21217424@gmail.com>
2023-04-08 11:24:56 +0800

    .

--- 1_🏠_Introduction.py
+++ 1_🏠_Introduction.py
@@ -2,11 +2,58 @@ import streamlit as st
 from functions import hide_elements, set_bg_image
 
 
+from pathlib import Path
+from streamlit.source_util import (
+    page_icon_and_name, 
+    calc_md5, 
+    get_pages,
+    _on_pages_changed
+)
+
+def delete_page(main_script_path_str, page_name):
+
+    current_pages = get_pages(main_script_path_str)
+    st.write(current_pages)
+
+    for key, value in current_pages.items():
+        if value['page_name'] == page_name:
+            del current_pages[key]
+            break
+        else:
+            pass
+    # _on_pages_changed.send()
+
+def add_page(main_script_path_str, page_name):
+    
+    pages = get_pages(main_script_path_str)
+    main_script_path = Path(main_script_path_str)
+    pages_dir = main_script_path.parent / "pages"
+    # st.write(list(pages_dir.glob("*.py"))+list(main_script_path.parent.glob("*.py")))
+    script_path = [f for f in list(pages_dir.glob("*.py"))+list(main_script_path.parent.glob("*.py")) if f.name.find(page_name) != -1][0]
+    script_path_str = str(script_path.resolve())
+    pi, pn = page_icon_and_name(script_path)
+    psh = calc_md5(script_path_str)
+    pages[psh] = {
+        "page_script_hash": psh,
+        "page_name": pn,
+        "icon": pi,
+        "script_path": script_path_str,
+    }
+    _on_pages_changed.send()
+
+
+if st.button("delete"):
+    delete_page("..\1_🏠_Introduction.py", "My_Dateset")
+if st.button("add"):
+     add_page("1_🏠_Introduction", "2_📃_My_Dateset")
+
+
+
 # 1. clear the cache
 st.session_state.current_country = "None"
 
 # 2. page config
-st.set_page_config(page_title="introduction-fpy", layout="centered")
+# st.set_page_config(page_title="introduction-fpy", layout="centered")
 
 # 2. import the css file
 with open("style.css") as css:
